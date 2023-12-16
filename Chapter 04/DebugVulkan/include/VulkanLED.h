4823ca886971ca5ca9451b9246aa6e9c331c3549
shulian <shuyan2@outlook.com>
2023-12-16 23:04:38 +0800

    populateDebugMessengerCreateInfo before vkCreateInstance or vkDestroyInstance will throw exception when enable validation layers, see another example code: https://vulkan-tutorial.com/code/02_validation_layers.cpp

--- Chapter 04/DebugVulkan/include/VulkanLED.h
+++ Chapter 04/DebugVulkan/include/VulkanLED.h
@@ -60,15 +60,6 @@ public:
 	VkBool32 areLayersSupported(std::vector<const char *> &layerNames);
 	VkResult createDebugReportCallback();
 	void	destroyDebugReportCallback();
-	static VKAPI_ATTR VkBool32 VKAPI_CALL debugFunction( VkFlags msgFlags, 
-														 VkDebugReportObjectTypeEXT objType,
-														 uint64_t srcObject, 
-														 size_t location, 
-														 int32_t msgCode,
-														 const char *layerPrefix,
-														 const char *msg, 
-														 void *userData);
-
 private:
 	PFN_vkCreateDebugReportCallbackEXT dbgCreateDebugReportCallback;
 	PFN_vkDestroyDebugReportCallbackEXT dbgDestroyDebugReportCallback;
